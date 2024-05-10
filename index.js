7292a7db570a380ec65a694341da97f463ac2510
Juya <juyaamirm@gmail.com>
2024-05-10 02:09:32 +0200

    ok

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
+import quotes from "./quotes.json" with { type: 'json' };
+
+console.log(quotes.quotes[2])
 
 const message = document.getElementById('message');
-const button = document.getElementById('button');
+const button = document.querySelector('#button');
 
-
-const qoutes = ['long life', 'be smart', 'go ahead ', 'go and get it', 'Sleep good'];
 button.addEventListener('click', ()=>{
     const p = randomNumber();
-
-    message.textContent = qoutes[p];
+    message.innerHTML = JSON.stringify(quotes.quotes[p].quote);
+    message.innerHTML += JSON.stringify(quotes.quotes[p].author);
 
 });
 
 
 function randomNumber(){
-     return Math.floor(Math.random()*5);
-}
+     return Math.floor(Math.random()*102);
+} 
 
 
-import quotes from "./quotes.json" assert { type: 'json' };
-console.log(quotes)
+
